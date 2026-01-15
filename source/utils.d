7b798ec787f0ab92457cb6cfd0579848eef74f90
dd86k <dd@dax.moe>
2026-01-15 10:51:27 -0500

    utils: avoid using toStringz, remove deprecated

--- source/utils.d
+++ source/utils.d
@@ -7,58 +7,57 @@ string sformatWindowsCode(char[] buffer, uint code)
     return cast(string)sformat(buffer, "0x%x", code);
 }
 
-deprecated("Use parseCode")
-bool unformatCode(ref uint code, const(char)[] input)
-{
-    import core.stdc.stdio : sscanf;
-    import std.string : toStringz;
-    
-    return sscanf(input.toStringz, "%i", cast(int*)&code) != 1;
-}
-unittest
-{
-    uint code = void;
-    assert(unformatCode(code, "0x20") == false);
-    assert(code == 0x20);
-}
-
 import core.stdc.stdio : sscanf;
-import std.string : toStringz;
 
+deprecated("Use parseCode(const(char)[], out uint)")
 uint parseCode(const(char)[] input, out bool success)
 {
+    char[32] buffer = void;
+    
+    if (input.length >= buffer.length)
+        throw new Exception("Buffer too small");
+    
+    buffer[0..input.length] = input[];
+    buffer[input.length] = 0;
+    
     uint code = void;
-    success = sscanf(input.toStringz, "%i", cast(int*)&code) >= 1;
+    success = sscanf(buffer.ptr, "%i", cast(int*)&code) >= 1;
     return code;
+}
+
+bool parseCode(const(char)[] input, out uint code)
+{
+    char[32] buffer = void;
+    
+    if (input.length >= buffer.length)
+        throw new Exception("Buffer too small");
+    
+    buffer[0..input.length] = input[];
+    buffer[input.length] = 0;
+    
+    return sscanf(buffer.ptr, "%i", cast(int*)&code) == 1;
 }
 unittest
 {
-    bool succ;
-    assert(parseCode("0", succ) == 0);
-    assert(succ);
-    assert(parseCode("5", succ) == 5);
-    assert(succ);
-    assert(parseCode("0x5", succ) == 0x5);
-    assert(succ);
-    assert(parseCode("0x1000", succ) == 0x1000);
-    assert(succ);
-    assert(parseCode("0x88884444", succ) == 0x8888_4444);
-    assert(succ);
-    assert(parseCode("0xC0000005", succ) == 0xC000_0005);
-    assert(succ);
-    assert(parseCode("6 5 4", succ) == 6);
-    assert(succ);
+    uint code;
+    assert(parseCode("0", code));
+    assert(code == 0);
+    assert(parseCode("5", code));
+    assert(code == 5);
+    assert(parseCode("0x5", code));
+    assert(code == 0x5);
+    assert(parseCode("0x1000", code));
+    assert(code == 0x1000);
+    assert(parseCode("0x88884444", code));
+    assert(code == 0x8888_4444);
+    assert(parseCode("0xC0000005", code));
+    assert(code == 0xC000_0005);
+    assert(parseCode("6 5 4", code));
+    assert(code == 6);
     
-    parseCode("Hello", succ);
-    assert(succ == false);
+    assert(parseCode("Hello", code) == false);
 }
 
-deprecated("Use plural(size_t,string,string)")
-pragma(inline, true)
-string plural(size_t count, string base, string singular, string multiple)
-{
-    return count == 1 ? base~singular : base~multiple;
-}
 string plural(size_t count, string single, string multi)
 {
     return count == 1 ? single : multi;
